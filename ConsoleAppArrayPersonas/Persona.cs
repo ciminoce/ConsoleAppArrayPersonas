c614bc9be5078a49ed71725d8e52a79893b901f5
Carlos Cimino <ciminoce@yahoo.com.ar>
2022-07-04 19:42:29 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleAppArrayPersonas/Persona.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleAppArrayPersonas
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public override string ToString()
+        {
+            return $"{Nombre}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj==null || !(obj is Persona))
+            {
+                return false;
+            }
+
+            return this.Nombre == ((Persona) obj).Nombre;
+        }
+    }
+}
---
+++ ConsoleAppArrayPersonas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace ConsoleAppArrayPersonas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personas = new Persona[]
+            {
+                new Persona() {Nombre = "David"},
+                new Persona {Nombre = "John"},
+                new Persona() {Nombre = "Mary"},
+                new Persona() {Nombre = "Brandy"}
+            };
+            MostrarContenido(personas);
+            Console.WriteLine($"Cantidad de personas: {personas.Length}");
+            Persona personaBuscada = new Persona() {Nombre = "David"};
+
+            bool existe = personas.Contains(personaBuscada);
+            Console.WriteLine(existe);
+
+            Console.ReadLine();
+        }
+
+        private static void MostrarContenido(Persona[] personas)
+        {
+            foreach (var persona in personas)
+            {
+                Console.WriteLine($"{persona}");
+            }
+        }
+    }
+}
